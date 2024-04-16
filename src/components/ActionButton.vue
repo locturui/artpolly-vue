6dc36399ba03a35593f56f48319219a6ac24613a
Stepan Leonov <s.leonov@innopolis.university>
2024-04-16 22:46:43 +0300

    Navbar

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -11,19 +11,20 @@ const props = defineProps<{
 <style scoped>
 .btn {
   background: none;
-	color: inherit;
-	border: none;
-	padding: 0;
-	font: inherit;
-	cursor: pointer;
-	outline: inherit;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
   border: 0.2em solid white;
   border-radius: 3em;
   padding: 1.2em 2em;
   font-family: Helvetica, sans-serif;
+  font-size: 14;
 }
-.nav-btn{
-  background-color: #FFFFFF;
+.nav-btn {
+  background-color: #ffffff;
   opacity: 90%;
   border-color: none;
 }
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 import ActionButton from './ActionButton.vue'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <div class="bar">
-    <div class="logo"></div>
+    <RouterLink to="/">
+      <div class="logo-wrapper">
+        <div class="logo"></div>
+        <span>Artpolly Studio</span>
+      </div>
+    </RouterLink>
+    <ul>
+      <li>Мастерклассы</li>
+      <li>Расписание</li>
+      <li>Контакты</li>
+      <li>Услуги</li>
+      <li>Наша продукция</li>
+    </ul>
     <ActionButton :classes="['btn', 'nav-btn']">Узнать больше</ActionButton>
   </div>
 </template>
@@ -14,17 +27,39 @@ import ActionButton from './ActionButton.vue'
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  background-color: #FFD0D4;
+  background-color: #ffd0d4;
   opacity: 80%;
   padding: 0.5em;
   width: 75%;
   margin: 1em auto;
   border-radius: 3em;
 }
+.logo-wrapper {
+  display: flex;
+  align-items: center;
+}
 .logo {
   background-color: white;
   border-radius: 50%;
   width: 4em;
   height: 4em;
+  margin-right: 0.7em;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  display: flex;
+  align-items: center;
+}
+
+li {
+  padding: 0 1em;
+}
+
+span {
+  font-family: Helvetica, sans-serif;
+  font-weight: bold;
+  font-size: 15;
 }
 </style>
