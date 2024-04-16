e865fb33d0fff1c61a571045ae42fb1c7a97b726
Stepan Leonov <s.leonov@innopolis.university>
2024-04-17 00:57:05 +0300

    aboutItem slot mechanics

--- src/components/AboutItem.vue
+++ src/components/AboutItem.vue
@@ -1,15 +1,21 @@
 <template>
-    <section>
-        <InfoItem></InfoItem>
-    </section>
+  <section>
+    <InfoItem imgUrl="/image/photo_about.jpeg" :classes="['']">
+      <template #header>О нас</template>
+      <template #content
+        >Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
+        IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum</template
+      >
+    </InfoItem>
+  </section>
 </template>
 <script setup>
-import InfoItem from "./InfoItem.vue"
+import InfoItem from './InfoItem.vue'
 </script>
 <style scoped>
-section{
-    width: 60%;
-    margin: auto;
-    padding-top: 7em;
+section {
+  width: 60%;
+  margin: auto;
+  padding-top: 7em;
 }
 </style>
--- src/components/InfoItem.vue
+++ src/components/InfoItem.vue
@@ -1,37 +1,39 @@
+<script setup lang="ts">
+const props = defineProps<{
+  imgUrl: string
+  classes: string[]
+}>()
+</script>
+
 <template>
-    <div class="about">
-        <div class="about-text">
-            <h1>О нас</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla efficitur enim iaculis, ultrices nisi non, tincidunt mi. Aenean ultricies consequat ipsum sit amet porttitor. Proin laoreet diam arcu, in semper dolor pulvinar sit amet. Nam mi justo, gravida quis enim ultrices, consectetur fringilla velit. Sed nec lacus maximus, dapibus neque vitae, dignissim quam. Phasellus eu eros justo. 
-                Donec rutrum sem erat, commodo consequat est vehicula vitae. </p>
-        </div>
-        <div class="image">
-            <!-- <img src="/image/photo_about.jpeg" alt="Фото кермачиского изделия" /> -->
-        </div>
+  <div class="about" :class="props.classes.join(' ')">
+    <div class="about-text">
+      <h1><slot name="header"></slot></h1>
+      <p><slot name="content"></slot></p>
     </div>
+    <img :src="props.imgUrl" alt="Фото кермачиского изделия" />
+  </div>
 </template>
 <style scoped>
-.about{
-    width: 100%;
-    display: flex;
-    flex-direction: row;
+.about {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
 }
 h1 {
   font-size: 2.5em;
   margin: 0;
 }
-.image{
-    width: 50% ;
-    margin: 0em 2em;
-    border-radius: 1.5em;
-    background-image: url("/image/photo_about.jpeg");
-    background-size:cover;
+img {
+  width: 50%;
+  margin: 0em 2em;
+  border-radius: 1.5em;
 }
-.about-text{
-    width: 50% ;
-    padding: 0em 2em;
+.about-text {
+  width: 50%;
+  padding: 0em 2em;
+}
+.reverse {
+  flex-direction: row-reverse;
 }
 </style>
-<script setup>
-</script>
