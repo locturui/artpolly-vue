143b0840468cb995d2f39719ec6f7819fe7a8f73
Viktoria Patrina <v.patrina@innopolis.university>
2024-04-16 19:52:25 +0300

    button style

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -8,4 +8,18 @@ const props = defineProps<{
   classes: string[]
 }>()
 </script>
-<style scoped></style>
+<style scoped>
+.btn {
+  background: none;
+	color: inherit;
+	border: none;
+	padding: 0;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
+  border: 0.2em solid blue;
+  border-radius: 3em;
+  padding: 1.2em 2em;
+  
+}
+</style>
