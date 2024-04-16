ccdf9670412080e5ac0a56ea2a2590860b7a63ad
Viktoria Patrina <v.patrina@innopolis.university>
2024-04-17 01:25:29 +0300

    icon

---
+++ src/components/IconItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="icon">
+      <img  src="/image/photo_about.jpeg" alt="Фото иконки" />
+      <h1><slot name="header"></slot></h1>
+      <p><slot name="content"></slot></p>
+    </div>
+</template>
+<style scoped>
+.icon{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+img{
+    width: 4em;
+    height: 4em;
+    border-radius: 100%;
+}
+h1{
+  font-size: 1.2em;
+  margin: 0;
+  margin-top: 1em;
+}
+</style>
+<script setup>
+</script>
--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -2,12 +2,17 @@
   <main class="home-wrapper">
     <MainItem></MainItem>
     <AboutItem></AboutItem>
+    <IconItem>
+      <template #header>Творчество рядом</template>
+      <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </template>
+    </IconItem>
   </main>
 </template>
 
 <script setup>
 import MainItem from './MainItem.vue'
 import AboutItem from './AboutItem.vue'
+import IconItem from './IconItem.vue'
 </script>
 
 <style scoped>
