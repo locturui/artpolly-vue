05ed4f5c29d14b3d6329788b5593f295258a9095
Stepan Leonov <s.leonov@innopolis.university>
2024-04-16 19:19:38 +0300

    first commit

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button :class="props.classes.join(' ')">
+    <slot></slot>
+  </button>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  classes: string[]
+}>()
+</script>
+<style scoped></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ActionButton from './ActionButton.vue'
+</script>
+
+<template>
+  <ActionButton :classes="['btn', 'red']">Узнать больше</ActionButton>
+</template>
+
+<style></style>
