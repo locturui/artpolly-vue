bc82af5653b1b9bc14c50bf15f9b231bec23e060
Stepan Leonov <s.leonov@innopolis.university>
2024-04-19 01:53:35 +0300

    fix event background

--- src/components/EventsSection.vue
+++ src/components/EventsSection.vue
@@ -1,50 +1,71 @@
 <template>
-    <secction class="box">
-        <div class="event-wrapper">
-            <h2>Ближайшие события</h2>
-            <div class="events">
-                <EventItem imgUrl="/image/photo_pot.jpeg">
-                    <template #name>Творчество рядом</template>
-                    <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </template>
-                </EventItem>
-                <EventItem imgUrl="/image/photo_pot.jpeg">
-                    <template #name>Творчество рядом</template>
-                    <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </template>
-                </EventItem>
-                <EventItem imgUrl="/image/photo_pot.jpeg">
-                    <template #name>Творчество рядом</template>
-                    <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </template>
-                </EventItem>
-                <EventItem imgUrl="/image/photo_pot.jpeg">
-                    <template #name>Творчество рядом</template>
-                    <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </template>
-                </EventItem>
-            </div>
+  <section>
+    <div class="bg">
+      <div class="fix"></div>
+    </div>
+    <div class="box">
+      <div class="event-wrapper">
+        <h2>Ближайшие события</h2>
+        <div class="events">
+          <EventItem imgUrl="/image/photo_pot.jpeg">
+            <template #name>Творчество рядом</template>
+            <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </template>
+          </EventItem>
+          <EventItem imgUrl="/image/photo_pot.jpeg">
+            <template #name>Творчество рядом</template>
+            <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </template>
+          </EventItem>
+          <EventItem imgUrl="/image/photo_pot.jpeg">
+            <template #name>Творчество рядом</template>
+            <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </template>
+          </EventItem>
+          <EventItem imgUrl="/image/photo_pot.jpeg">
+            <template #name>Творчество рядом</template>
+            <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </template>
+          </EventItem>
         </div>
-    </secction>
+      </div>
+    </div>
+  </section>
 </template>
 <script setup>
 import EventItem from './EventItem.vue'
 </script>
 <style scoped>
-.box{
-    background-color: #83A165;
-    margin-top: 2em;
+section {
+  margin-top: 3em;
 }
-h2{
+.bg {
+  position: relative;
+  background-image: url('/image/bg-2.svg');
+  width: 100%;
+  padding-top: 5.25%;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+.fix {
+  position: absolute;
+  bottom: 0;
+  background-color: #83a165;
+  height: 1px;
+  width: 100%;
+}
+.box {
+  background-color: #83a165;
+}
+h2 {
   color: white;
   text-align: center;
-  margin: 1em 0 0 0;
+  margin: 0;
 }
-.events{
+.events {
   display: flex;
   justify-content: space-between;
   padding: 2em;
   color: white;
 }
 
-.event-wrappers{
-  
+.event-wrappers {
   display: flex;
   justify-content: center;
   align-items: center;
